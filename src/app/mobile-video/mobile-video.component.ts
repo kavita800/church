6f9ce69a96cd44769a8f408b82e8b60565cbcd2a
kavita800 <arbaz@cantech.co>
2020-08-26 11:24:00 +0530

    Update landing page

--- src/app/mobile-video/mobile-video.component.ts
+++ src/app/mobile-video/mobile-video.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-video',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileVideoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router ,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
